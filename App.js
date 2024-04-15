eaaa7782c903fde4f0c3b1e9fc7866e3c72ff331
Your Name <you@example.com>
2024-04-15 20:32:37 +0700

    Test

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Pressable,
+  Modal,
+  StatusBar,
+  SectionListComponent,
+} from "react-native";
+import { useState } from "react";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <StatusBar backgroundColor="lightgreen" barStyle="dark-content" />
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Button
+          title="Press"
+          onPress={() => setModalVisible(true)}
+          color="midnightblue"
+        />
+        <Modal
+          visible={isModalVisible}
+          onRequestClose={() => setIsModalVisible(false)}
+          animationType="fade"
+          presentationStyle="pageSheet">
+          <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+            <Text>Model Content</Text>
+            <Button
+              title="Close"
+              color="midnightblue"
+              onPress={() => setModalVisible(false)}
+            />
+          </View>
+        </Modal>
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
